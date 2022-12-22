d74ec677fa0af4d58709e2e0d62b3b10ccf50926
Chris <ctaylor83@gmail.com>
2022-12-22 19:55:51 +0000

    Problems 1, 2, 3 Complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    // If a negative number is input return an error
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let output = '';
+    // For each number return the input string that number of times
+    for (let i = 0; i < num; i++) {
+        output += str;
+    }
+    return output;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    // Creating array for string to go into
+    const revStr = [];
+    const length = str.length - 1;
+
+    // Looping from the end to reverse string
+    for (let i = length; i >= 0; i--) {
+        revStr.push(str[i]); 
+    } 
+    // Join it all back up together but in reverse!
+    return revStr.join('');
 };
 
 // Do not edit below this line
