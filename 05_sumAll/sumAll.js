b1b50d08b37ea06ad337dea41ae1519a1e90946d
Chris <ctaylor83@gmail.com>
2022-12-28 19:37:47 +0000

    Solution to problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // Returns 'ERROR' if an non numeric input is selected
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return('ERROR');
+    }
 
+    // Returns 'ERROR' if a negative number is selected
+    if (a < 0 || b < 0) {
+        return('ERROR');
+    }
+
+    // Swap a & b if a is selected as the larger number than b
+    if (a > b) {
+        let tempNo = a;
+        a = b;
+        b = tempNo;
+    }
+
+    // Add all numbers up between and including a & b
+    let sum = 0
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    // Returns the output of the above calculation
+    return sum
 };
 
 // Do not edit below this line
