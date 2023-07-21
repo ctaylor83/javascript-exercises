b4ef7d306de82c3c2257708a429a0144d8da8bb3
Chris Taylor <ctaylor83@gmail.com>
2023-07-21 12:52:56 +0100

    Completion of exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n <= 0) {
+        return "OOPS";
+    } else if (n === 1 || n === 2) {
+        return 1;
+    } else {
+        let fib = [1, 1];
+        for (let i = 2; i < n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2]
+        }
+        return fib[n - 1];
+    }
 
 };
 
