ffbbc9313f04c0144769a993c67c61155326292d
Chris Taylor <ctaylor83@gmail.com>
2023-07-21 12:46:41 +0100

    Updated exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,7 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleaned = str.replace(/[\W_]/g, '').toLowerCase();
+    let reversed = cleaned.split('').reverse().join('');
+    return cleaned === reversed;
 
 };
 
