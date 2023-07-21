ee51724eb87af06e401871e757983f031b2d45ff
Chris Taylor <ctaylor83@gmail.com>
2023-07-21 13:49:57 +0100

    Completion of exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,40 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const currentYear = new Date().getFullYear();
+        const oldAge = oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : currentYear - oldest.yearOfBirth;
+        const personAge = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : currentYear - person.yearOfBirth;
+        return oldAge < personAge ? person : oldest;
+    });
 };
+
+const people = [
+    {
+      name: 'Carly',
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: 'Ray',
+      yearOfBirth: 1962,
+      yearOfDeath: 2011
+    },
+    {
+      name: 'Jane',
+      yearOfBirth: 1912,
+      yearOfDeath: 1941
+    },
+    {
+      name: 'Bob',
+      yearOfBirth: 1920,
+      yearOfDeath: 2000
+    },
+    {
+      name: 'Chris',
+      yearOfBirth: 1980
+    }
+  ];
+  
+  console.log(findTheOldest(people)); 
 
 // Do not edit below this line
 module.exports = findTheOldest;
